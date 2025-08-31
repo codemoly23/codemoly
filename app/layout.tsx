54bcfe32feeca66fe9bcf699aa091c6ad4849e2e
Md Sajeeb Hossain <naziullahshawn135@gmail.com>
2025-08-31 16:59:52 +0600

    implement google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import ScrollToTop from "@/components/ui/ScrollToTop";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -79,6 +80,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleTagManager gtmId="GTM-NT98MXWS" />
         {children}
         <ScrollToTop />
       </body>
